57ce72e1eb980fd77fa0b80f3e9133cab610dbb7
GaryLehigh <xig514@lehigh.edu>
2019-04-22 09:56:24 +0800

    Create ChineseCharacters.py

---
+++ ChineseCharacters.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+'''
+Created on 
+
+@author: g00377630
+'''
+xiaoqu = [u"我喜欢",u"你",u"我喜欢你", u"我喜欢她", u"我喜欢她们",  u"我喜欢你们", u"我喜欢他们", u"我喜欢大家", u"我喜欢正常"]
+#print (xiaoqu)
+unicodeList =''
+for word in xiaoqu:
+    unicodeList = unicodeList + word
+    
+map = {1:2, 3:4,4:5}
+#if 5 in map:
+    #print(map[1])
